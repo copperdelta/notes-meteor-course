bd8fe745c953885c22b3971fa0ea7bd525f62602
DEAN <dean@copperdelta.com>
2018-01-20 17:36:11 -0500

    Render Empty Item if no notes

---
+++ imports/ui/NoteListEmptyItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoteListEmptyItem = () => {
+  return (
+    <div>
+      <h5>You have No Notes</h5>
+      <p>Create a new note tp get started!</p>
+    </div>
+  );
+};
+
+export default NoteListEmptyItem;
